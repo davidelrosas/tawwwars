40144d60e3052c67682ceec4bb8c0001b3a82002
pyromelonz <pyromelonz@gmail.com>
2024-03-14 12:11:12 +0100

    cleaned up code and added enemy spawn scheduling

--- scripts/Beam.gd
+++ scripts/Beam.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-var rps : float = 3
-var active_angle : float = PI/6
+@export_group("Beam Properties")
+@export var rps : float = 3
+@export var active_angle : float = PI/6
 @export var working_range : float = 800
 
 func set_bpm(bpm):
@@ -14,14 +15,11 @@ func angle_to(pos : Vector2) -> float:
 	return beam_normal().angle_to(global_position-pos)
 
 func in_zone(pos : Vector2) -> bool:
-	
 	return (abs(angle_to(pos)) <= active_angle) and (pos-global_position).length_squared() < working_range*working_range
 	
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	rotate(PI * delta * rps)
 	pass
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,12 +1,23 @@
 extends Node2D
 
-var period = 2
 var elapsed = 0
-var spawnarea = position
+var spawnarea = global_position
 @export var enemy_target : Vector2 = Vector2(0,0)
 @export var concurrent_spawns : int = 10
 
+@export var spawn_schedule : String = "mob:3,mib:1#2/mob:1,mib:1#1"
+@export var loop_spawns : bool
+
+var sp_sch
+
+var schedule_index = 0
+
 var spawned = []
+
+static var enemy_types = {
+	"mob" : preload("res://prefabs/enemy.tscn"),
+	"mib" : preload("res://prefabs/enemy.tscn")
+}
 
 func n_can_spawn() -> int:
 	return concurrent_spawns - spawned.size()
@@ -14,18 +25,15 @@ func n_can_spawn() -> int:
 # spawnlist example: "mob:3,orc:2"
 func spawn_enemy(spawnlist, target):
 	spawnlist = spawnlist.split(",")
-	var proto
+	var newmob_type
 	for x in spawnlist:
 		var spawngroup = x.split(":")
-		match spawngroup[0]:
-			"mob":
-				proto = preload("res://prefabs/enemy.tscn")
-			"mib":
-				proto = preload("res://prefabs/enemy.tscn")
-			_:
-				print("invalid enemy type")
+		if enemy_types.has(spawngroup[0]):
+			newmob_type = enemy_types[spawngroup[0]]
+		else:
+			print("trying to spawn invalid enemy type")
 		for y in range(min(spawngroup[1].to_int(),n_can_spawn())):
-			var newmob = proto.instantiate()
+			var newmob = newmob_type.instantiate()
 			newmob.global_position = spawnarea
 			newmob.target = enemy_target
 			spawned.push_back(newmob)
@@ -33,17 +41,26 @@ func spawn_enemy(spawnlist, target):
 	pass
 	
 func spread(enemylist):
-	
 	pass
 	
-# Call this function to spawn a new node
+func set_spawnschedule(schedule : String):
+	sp_sch = schedule.split("/")
+	pass
+	
 func _ready():
+	set_spawnschedule(spawn_schedule)
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	elapsed += delta
-	if (elapsed>=period):
-		elapsed-= period
-		spawn_enemy("mob:3,mib:1",enemy_target)
+	
+	if (schedule_index < sp_sch.size()):
+		var cur = sp_sch[schedule_index].split("#")
+		var curperiod = cur[1].to_float()
+		if curperiod < elapsed:
+			elapsed -= curperiod
+			spawn_enemy(cur[0],enemy_target)
+			schedule_index += 1
+			if loop_spawns and schedule_index >= sp_sch.size():
+				schedule_index -= sp_sch.size()
 	pass
