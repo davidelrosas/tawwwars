5fbf13b88e4df68143d2c15c4e7118ca4caaf4a0
pyromelonz <pyromelonz@gmail.com>
2024-04-12 13:56:03 +0200

    fixed bpm for beam and pizza

--- prefabs/manapool.gd
+++ prefabs/manapool.gd
@@ -11,11 +11,7 @@ func on_beat_advance(beat : int):
 	var pizza = $"Pizza"
 	var beam = $"Beam"
 	beam.rotation = pizza.get_beat_angle()
-<<<<<<< HEAD
 	beam.rps = pizza.angle / (pizza.division * pizza.interval * TAU * (-2 * int(pizza.reverse)+1))
-=======
-	beam.set_speed(pizza.bpm / pizza.get_swing_multiplier() * (int(pizza.reverse) * -2 + 1) * pizza.angle/TAU, pizza.division)
->>>>>>> 8a42e7c (added rythm pizza and manapool, which combines it with beam)
 
 func _process(delta):
 	pass
