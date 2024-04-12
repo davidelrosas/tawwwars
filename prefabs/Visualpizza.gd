f1df6ac42f9b5f5088f66fa0c927842ce96c4aee
pyromelonz <pyromelonz@gmail.com>
2024-04-12 12:35:03 +0200

    added rythm pizza and manapool, which combines it with beam

---
+++ prefabs/Visualpizza.gd
@@ -0,0 +1,12 @@
+extends Line2D
+
+func redraw(start_degr : float, division : int, radius:int, angle : float):
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
