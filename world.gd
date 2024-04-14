876eb032552e82217bc63020e355f26d393cad05
davidelrosas <161586949+davidelrosas@users.noreply.github.com>
2024-04-14 13:56:24 +0200

    changed main scene got rid of useless stuff

---
+++ world.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var towar = Towar.new(Towar.towar_model.TURRET)
+var mob = Mob.new(Mob.hostile_mob.BOSSMAN)
+
+func _ready():
+	mob.position = Vector2(1000,1000)
+	towar.position = Vector2(500,100)
+	add_child(towar)
+	add_child(mob)
+	
+	#will be handled better by targeting system
+	towar.target_data.closest = mob.get_child(0)
+	
+
+func _physics_process(delta):
+	pass
